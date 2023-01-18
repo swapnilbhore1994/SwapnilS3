008df96c5c52ede8cbcffea6ccf9d032f78775fa
SwapnilS3 <swapnilbhore1994@gmail.com>
2023-01-19 00:55:28 +0530

    My first commit

---
+++ src/main/java/com/bankproject/base/Baseclass.java
@@ -0,0 +1,18 @@
+package com.bankproject.base;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Baseclass {
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","D:\\Selenium\\chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://demo.guru99.com/V1/index.php");
+ 
+		
+		
+ }
+}
